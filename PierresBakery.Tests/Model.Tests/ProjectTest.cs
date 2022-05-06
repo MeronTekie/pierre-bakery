51fb363c7765d64c2e654419ddf7efcbe3ebed0d
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-05-06 09:52:36 -0700

    First test passes

---
+++ PierresBakery.Tests/Model.Tests/ProjectTest.cs
@@ -0,0 +1,25 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using Pastry.Models;
+// using PierresBakery.Models;
+using Program.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class PriceTests
+  {
+    [TestMethod]
+    public void FirstTest_TotalPice()
+    {
+      
+
+    }
+
+  }
+}
+
+
+
